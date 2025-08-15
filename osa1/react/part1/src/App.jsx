f087f8eb497530ffa28c501462ee25139daaa49b
Smuli99 <hytonensamu99@gmail.com>
2025-08-15 13:42:02 +0300

    Osa 1 tehtävä 1.2 tehty valmiiksi.

--- osa1/react/part1/src/App.jsx
+++ osa1/react/part1/src/App.jsx
@@ -4,9 +4,19 @@ const Header = (props) => {
   );
 };
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>{props.part} {props.exercise}</p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
   );
 };
 
@@ -30,9 +40,11 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
+      <Content 
+        part1={part1} exercise1={exercises1}
+        part2={part2} exercise2={exercises2}
+        part3={part3} exercise3={exercises3}
+      />
       <Total content={totalContent} exercises={totalExercises} />
     </div>
   );
