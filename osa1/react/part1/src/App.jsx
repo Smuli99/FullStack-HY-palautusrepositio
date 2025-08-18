f7b695476ff734092b2e2878ca15f791e31b1353
Smuli99 <hytonensamu99@gmail.com>
2025-08-18 12:18:15 +0300

    Tehtävä 1.6 aloitettu

--- osa1/react/part1/src/App.jsx
+++ osa1/react/part1/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+/*
 const Header = (props) => {
   return (
     <h1>{props.course.name}</h1>
@@ -29,6 +32,19 @@ const Total = (props) => {
   );
 };
 
+
+const Display = ({ counter }) => {
+  return (
+    <p>{counter}</p>
+  );
+};
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  );
+};
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
@@ -48,11 +64,65 @@ const App = () => {
     ]
   };
 
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => setCounter(counter + 1);
+  const reset = () => setCounter(0);
+
   return (
     <div>
       <Header course={course} />
       <Content part={course.parts} />
       <Total part={course.parts} />
+      <Display counter={counter} />
+      <Button onClick={increase} text="Increase by one" />
+      <Button onClick={reset} text="Reset" />
+    </div>
+  );
+};
+*/
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing buttons
+      </div>
+    );
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  );
+};
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+
+  const handleLeftClick = () => {
+    setAllClicks(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
+
+  const handleRightClick = () => {
+    setAllClicks(allClicks.concat('R'));
+    setRight(right + 1);
+  };
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text='left' />
+        <Button onClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   );
 };
