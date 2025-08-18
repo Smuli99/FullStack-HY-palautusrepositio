fe9d33821aa7c38e9c0fede54c81c1c9747eec22
Smuli99 <hytonensamu99@gmail.com>
2025-08-19 00:55:01 +0300

    Tehtävä 2.8 tehty valmiiksi

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,12 +1,42 @@
 import { useState } from 'react';
 
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const NewPersonForm = ({ addName, newName, handleNewName, newNumber, handleNewNumber }) => {
+  return (
+    <form onSubmit={addName}>
+      <div>
+        name: <input value={newName} onChange={handleNewName} required="required" />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNewNumber} />
+      </div>
+        <button type="submit">add</button>
+    </form>
+  );
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <ul>
+      {persons.map(person =>
+        <li key={person.name}>
+          {person.name} {person.number || '-no number-'}
+        </li>
+      )}
+    </ul>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     {
-      name: 'Arto Hellas'
+      name: 'Arto Hellas',
+      number: '040-123456'
     }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
@@ -18,36 +48,29 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
     } else {
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber.trim() === '' ? '' : newNumber
       };
       
       setPersons(persons.concat(personObject));
     }
     
     setNewName('');
+    setNewNumber('');
   }
-
-  const handleNewName = (event) => setNewName(event.target.value);
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} required="required" />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-        <ul>
-          {persons.map((person) => (
-            <li key={person.name}>
-              {person.name}
-            </li>
-          ))}
-        </ul>
+      <Header text="Phonebook" />
+      <NewPersonForm 
+        addName={addName}
+        newName={newName}
+        handleNewName={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNewNumber={(e) => setNewNumber(e.target.value)}
+      />
+      <Header text="Numbers" />
+      <Persons persons={persons} />
     </div>
   );
 };
