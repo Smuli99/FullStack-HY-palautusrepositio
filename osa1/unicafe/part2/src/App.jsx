f40749d7426d74640d39a8933a4d071fb6c29474
Smuli99 <hytonensamu99@gmail.com>
2025-08-18 12:57:15 +0300

    Tehtävä 1.7 tehty valmiiksi

--- osa1/unicafe/part2/src/App.jsx
+++ osa1/unicafe/part2/src/App.jsx
@@ -5,11 +5,18 @@ const Header = ({ text }) => <h1>{text}</h1>;
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total; // return 0 if total is 0 else calculate average
+  const positive = total === 0 ? 0 : (good / total) * 100; // return 0 if total is 0 else calculate positive percentage
+  
   return (
     <>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive} %</p>
     </>
   );
 };
