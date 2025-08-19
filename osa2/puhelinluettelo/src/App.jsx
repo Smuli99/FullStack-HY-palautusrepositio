172742c7f6274fcf754c8c40eed95adf1e56faa2
Smuli99 <hytonensamu99@gmail.com>
2025-08-19 11:32:22 +0300

    Tehtävä 2.11 tehty valmiiksi

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Header = ({ text }) => <h1>{text}</h1>;
 
@@ -41,15 +42,18 @@ const NewPersonForm = ({ addName, newName, handleNewName, newNumber, handleNewNu
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6425922' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() =>{
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault();
