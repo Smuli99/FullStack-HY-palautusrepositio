863ade3356dc24781a94a5b6a7d1f2445a2828ac
Smuli99 <hytonensamu99@gmail.com>
2025-08-22 10:05:14 +0300

    Tehtävä 3.5 tehty valmiiksi

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
     {
         id: "1",
@@ -51,6 +53,29 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(p => p.id !== id);
 
     response.status(204).end();
+});
+
+const generateId = () => String(Math.floor(Math.random() * 1000));
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        });
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number || '--no number--'
+    };
+
+    persons = persons.concat(person);
+
+    console.log(person);
+    response.json(person);
 });
 
 const PORT = 3001;
