3c990866bb61c7a7928b4dc245d5300000dd875d
Smuli99 <hytonensamu99@gmail.com>
2025-08-25 10:50:26 +0300

    Notes muutettu

--- osa3/notes/index.js
+++ osa3/notes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 let notes = [
     {
         id: "1",
@@ -87,7 +90,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
