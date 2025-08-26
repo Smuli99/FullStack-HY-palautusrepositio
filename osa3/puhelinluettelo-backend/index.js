6f5990df1168e4290079e0e304a43c380a6ea2d4
Smuli99 <hytonensamu99@gmail.com>
2025-08-26 12:39:24 +0300

    Tehtävä 3.13 tehty valmiiksi

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,29 +1,9 @@
+require('dotenv').config();
+const Person = require('./models/person');
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-];
+const app = express();
 
 app.use(express.json());
 app.use(express.static('dist'));
@@ -37,26 +17,37 @@ morgan.token('body', (request) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({})
+        .then(persons => {
+            response.json(persons);
+        });
 });
 
 app.get('/info', (request, response) => {
-    const totalPersons = persons.length;
-    const date = new Date();
-    response.send(`
-        <p>Phonebook has info for ${totalPersons} people</p>
-        <p>${date}</p>
-    `);
+    Person.countDocuments({})
+        .then(count => {
+            const date = new Date();
+            response.send(`
+                <p>Phonebook has info for ${count} people</p>
+                <p>${date}</p>
+            `);
+        });
 });
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id;
-    const person = persons.find(p => p.id === id);
-
-    if (person) response.json(person);
-    else response.status(404).end();
+    
+    Person.findById(id)
+        .then(person => {
+            if (person) response.json(person);
+            else response.status(404).end();
+        })
+        .catch(error => {
+            console.log('error finding person:', error);
+        });
 });
 
+// TODO:
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id;
     console.log(request.params);
@@ -65,8 +56,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 });
 
-const generateId = () => String(Math.floor(Math.random() * 1000));
-
+// TODO:
 const dublicateName = (name) => {
     return persons.some(p => 
         p.name.toLowerCase() === name.trim().toLowerCase()
@@ -84,25 +74,27 @@ app.post('/api/persons', (request, response) => {
 
     const name = body.name.trim();
 
+    // TODO:
+    /*
     if (dublicateName(name)) {
         return response.status(409).json({
             error: 'name must be unique'
         });
     }
+    */
 
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: name,
-        number: body.number
-    };
+        number: body.number,
+    });
 
-    persons = persons.concat(person);
-
-    console.log('Added', person);
-    response.json(person);
+    person.save().then(savedNote => {
+        response.json(savedNote);
+        console.log('Added', person);
+    });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
---
+++ osa3/puhelinluettelo-backend/models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+console.log('connection to', url);
+mongoose.connect(url)
+    .then(response => {
+        console.log('connected to MongoDB!');
+    })
+    .catch(error => {
+        console.log('error connection to MongoDB failed:', error);
+    });
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+
+module.exports = mongoose.model('Person', personSchema);
