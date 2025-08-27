de11150bfa3f1162019ba950bfa5442c87c4ac3f
Smuli99 <hytonensamu99@gmail.com>
2025-08-27 10:12:16 +0300

    Notes päivitetty

--- osa3/notes/index.js
+++ osa3/notes/index.js
@@ -49,7 +49,7 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error));
 });
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body;
 
     if (!body.content) {
@@ -65,7 +65,8 @@ app.post('/api/notes', (request, response) => {
 
     note.save().then(savedNote => {
         response.json(savedNote);
-    });
+    })
+    .catch(error => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -99,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).sen({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
     }
 
     next(error);
