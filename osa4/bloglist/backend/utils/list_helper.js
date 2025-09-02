62717a8d3c3b9a3adadff39b685b009791752ce0
Smuli99 <hytonensamu99@gmail.com>
2025-09-02 16:43:24 +0300

    Tehtävä 4.4 tehty valmiiksi

--- osa4/bloglist/backend/utils/list_helper.js
+++ osa4/bloglist/backend/utils/list_helper.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-const dummy = (blogs) => {
-  const result = 1;
-  return result;
-};
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => (
+  blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+);
 
 module.exports = {
   dummy,
+  totalLikes,
 };
