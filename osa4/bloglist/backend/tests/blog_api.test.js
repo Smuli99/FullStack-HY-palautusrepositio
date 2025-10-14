fa09e4216e3a383e055bc55b8a1f92ddcd5e3c35
Smuli99 <hytonensamu99@gmail.com>
2025-10-14 13:55:59 +0300

    Tehtävä 4.9 tehty valmiiksi

--- osa4/bloglist/backend/tests/blog_api.test.js
+++ osa4/bloglist/backend/tests/blog_api.test.js
@@ -26,6 +26,15 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
+test('blogs unique identifier is named id not _id', async () => {
+  const response = await api.get('/api/blogs');
+  const blog = response.body[0];
+
+  assert.ok(blog.id);
+  // eslint-disable-next-line no-underscore-dangle
+  assert.strictEqual(blog._id, undefined);
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
