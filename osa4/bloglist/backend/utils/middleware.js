0cdb27506a6282484fb32ed7a895e4e3cbd61499
Smuli99 <hytonensamu99@gmail.com>
2025-10-16 19:57:48 +0300

    Tehtävään 4.20 tehty valmiiksi

--- osa4/bloglist/backend/utils/middleware.js
+++ osa4/bloglist/backend/utils/middleware.js
@@ -34,8 +34,21 @@ const errorHandler = (error, request, response, next) => {
   return next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.replace('Bearer ', '');
+  } else {
+    request.token = null;
+  }
+
+  next();
+};
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
