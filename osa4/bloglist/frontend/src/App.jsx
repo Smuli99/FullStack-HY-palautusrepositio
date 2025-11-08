933cdea65cbb3033a06f23db27148a3528f253c2
Smuli99 <hytonensamu99@gmail.com>
2025-11-08 18:27:13 +0200

    Tehtävä 5.2 tehty valmiiksi

--- osa4/bloglist/frontend/src/App.jsx
+++ osa4/bloglist/frontend/src/App.jsx
@@ -17,6 +17,15 @@ const App = () => {
     );
   }, []);
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
   const loginScreen = () => (
     <div>
       <h2>log in to application</h2>
@@ -51,7 +60,8 @@ const App = () => {
   const showBlogs = () => (
     <div>
       <h2>blogs</h2>
-      <p>{user.username} logged in</p>
+      <p>{user.name} logged in</p>
+      <button onClick={() => handleLogout()}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
@@ -64,6 +74,10 @@ const App = () => {
     try {
       const user = await loginService.login({ username, password });
       
+      window.localStorage.setItem(
+        'loggedBlogAppUser', JSON.stringify(user)
+      );
+
       blogService.setToken(user.token);
   
       setUser(user);
@@ -75,6 +89,12 @@ const App = () => {
         setErrorMessage(null);
       }, 5000);
     }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogAppUser');
+    setUser(null);
+    blogService.setToken(null);
   };
 
   return user ? showBlogs() : loginScreen();
