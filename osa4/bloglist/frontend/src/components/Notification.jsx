a9a8ad4d047297ad7c1659d0ed1fd4d86ca72a37
Smuli99 <hytonensamu99@gmail.com>
2025-11-09 01:46:36 +0200

    Komponentti ilmoituksille tehty

---
+++ osa4/bloglist/frontend/src/components/Notification.jsx
@@ -0,0 +1,21 @@
+const Notification = ({ notification }) => {
+  if (!notification) return null;
+
+  const style = {
+    color: notification.type === 'error' ? 'red' : 'green',
+    background: 'lightgrey', 
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  };
+
+  return (
+    <p style={style}>
+    {notification.message}
+    </p>
+  );
+};
+
+export default Notification;
