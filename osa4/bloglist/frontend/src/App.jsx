446f03076bdb1fee0a31332f1159c2dbb8a52d29
Smuli99 <hytonensamu99@gmail.com>
2025-11-08 18:19:39 +0200

    Tehtävä 5.1 tehty valmiiksi

--- osa4/bloglist/frontend/src/App.jsx
+++ osa4/bloglist/frontend/src/App.jsx
@@ -1,24 +1,83 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import ErrorMessage from './components/ErrorMessage'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
-  }, [])
+    );
+  }, []);
 
-  return (
+  const loginScreen = () => (
+    <div>
+      <h2>log in to application</h2>
+      <ErrorMessage message={errorMessage} />
+      
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+
+  const showBlogs = () => (
     <div>
       <h2>blogs</h2>
+      <p>{user.username} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
 
-export default App
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    
+    try {
+      const user = await loginService.login({ username, password });
+      
+      blogService.setToken(user.token);
+  
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch {
+      setErrorMessage('wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  return user ? showBlogs() : loginScreen();
+};
+
+export default App;
