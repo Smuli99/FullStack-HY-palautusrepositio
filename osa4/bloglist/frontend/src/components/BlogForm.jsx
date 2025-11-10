6fbb271c7e70b16fad3c3532e98a8e6f3fa17b01
Smuli99 <hytonensamu99@gmail.com>
2025-11-10 16:20:03 +0200

    muutettu toteutusta

--- osa4/bloglist/frontend/src/components/BlogForm.jsx
+++ osa4/bloglist/frontend/src/components/BlogForm.jsx
@@ -16,42 +16,46 @@ const BlogForm = ({ handleNewBlog }) => {
   };
 
   return (
-    <form onSubmit={addBlog}>
-      <div>
-        <label>
-          title:
-          <input 
-            type="text"
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          author:
-          <input 
-            type="text"
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          url:
-          <input 
-            type="text"
-            value={url}
-            onChange={({ target }) => setUrl(target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <button type="submit">create</button>
-      </div>
-    </form>
-  )
+    <div>
+      <h2>create new</h2>
+
+      <form onSubmit={addBlog}>
+        <div>
+          <label>
+            title:
+            <input 
+              type="text"
+              value={title}
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            author:
+            <input 
+              type="text"
+              value={author}
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            url:
+            <input 
+              type="text"
+              value={url}
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <button type="submit">create</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default BlogForm;
