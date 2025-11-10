01fc18325bcbc990b330f16541027e202af32310
Smuli99 <hytonensamu99@gmail.com>
2025-11-10 16:19:50 +0200

    muutettu toteutusta

--- osa4/bloglist/frontend/src/components/LoginForm.jsx
+++ osa4/bloglist/frontend/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
+import Notification from "./Notification"
 
-const LoginForm = ({ handleLogin }) => {
+const LoginForm = ({ handleLogin, notification }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   
@@ -12,29 +13,34 @@ const LoginForm = ({ handleLogin }) => {
   };
 
   return (
-    <form onSubmit={login}>
-      <div>
-        <label>
-          username
-          <input
-            type="text"
-            value={username}
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          password
-          <input
-            type="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </label>
-      </div>
-      <button type="submit">login</button>
-    </form>
+    <div>
+      <h2>log in to application</h2>
+      <Notification notification={notification} />
+      
+      <form onSubmit={login}>
+        <div>
+          <label>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
   );
 };
 
