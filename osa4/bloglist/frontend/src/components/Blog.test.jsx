f69e0fb40b232fbe29879fedb09383005736476a
Smuli99 <hytonensamu99@gmail.com>
2025-11-11 20:14:03 +0200

    Tehtävä 5.14 tehty valmiiksi

--- osa4/bloglist/frontend/src/components/Blog.test.jsx
+++ osa4/bloglist/frontend/src/components/Blog.test.jsx
@@ -30,4 +30,14 @@ describe('<Blog />', () => {
     expect(screen.queryByText('likes 5')).toBeNull();
     expect(screen.queryByText('Superuser')).toBeNull();
   });
+
+  test('renders url, likes and user when view button is clicked', async () => {
+    const user = userEvent.setup();
+    const button = screen.getByText('view');
+    await user.click(button);
+
+    expect(screen.getByText('http://testblog.com')).toBeDefined();
+    expect(screen.getByText('likes 5')).toBeDefined();
+    expect(screen.getByText('Superuser')).toBeDefined();
+  });
 });
