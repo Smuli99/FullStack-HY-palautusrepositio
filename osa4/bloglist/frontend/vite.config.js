8a394bc3b9709cf5d2e83a0a51536e90e8551458
Smuli99 <hytonensamu99@gmail.com>
2025-11-11 18:37:03 +0200

    testi konfiguraatiot tehty

--- osa4/bloglist/frontend/vite.config.js
+++ osa4/bloglist/frontend/vite.config.js
@@ -1,15 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { globals } from 'vitest';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:3003',
-        changeOrigin: true
-      }
-    }
-  }
+        changeOrigin: true,
+      },
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.js',
+    include: ['**/*.test.{js,jsx}'], // files -> include
+    environmentOptions: {
+      jsdom: {
+        globals: {
+          ...globals.vitest,
+        },
+      },
+    },
+  },
 });
