56316fe3cf78c929c376e47bbd6b3105fc31aa60
Smuli99 <hytonensamu99@gmail.com>
2025-11-17 10:37:47 +0200

    muutettu

--- osa4/bloglist/frontend/src/components/Blog.jsx
+++ osa4/bloglist/frontend/src/components/Blog.jsx
@@ -6,16 +6,8 @@ const Blog = ({ blog, handleAddLike, handleRemoveBlog, loggedUser }) => {
   const toggleVisibility = () => setDetailsVisible(!detailsVisible);
   const isCreator = () => loggedUser && blog.user.username === loggedUser.username;
 
-  const blogStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  };
-
   return (
-    <div style={blogStyle}>
+    <div className="blog">
       <div>
         {blog.title} {blog.author}
         <button onClick={toggleVisibility}>
